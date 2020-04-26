9c2121acad7fc030a02ba914816b62d209ea54fa
tomerror1 <tom_klinge@outlook.de>
2020-04-26 11:17:07 +0200

    adding templates and layout

--- fined_flask.py
+++ fined_flask.py
@@ -1,13 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': "Tom K.",
+        'title': 'The Backend day',
+        'date_posted': 'April 20, 2020',
+    },
+    {
+        'author': "Tomas G.",
+        'title': 'The Frontend Day',
+        'date_posted': 'April 19, 2020',
+    }
+]
 
 @app.route('/')
 def hello_world():
-    return '<h1>Thats nice!</h1>'
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    return '<h1>About page<h1>'
+    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
