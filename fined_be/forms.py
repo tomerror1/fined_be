5e77e921a06be323d85ea4af8d15023a5c84f357
tomerror1 <tom_klinge@outlook.de>
2020-04-30 20:10:07 +0200

    update username and email at account page with a form

--- fined_be/forms.py
+++ fined_be/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from fined_be.models import User
@@ -36,3 +37,26 @@ class LoginForm(FlaskForm):
         validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', 
+        validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('E-Mail', 
+        validators=[DataRequired(), Email()])
+    submit = SubmitField('Update')
+
+    # only check if data is different from current user
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            # if user is anything else then none (=True), then i want an error
+            if user:
+                raise ValidationError('Dieser Benutzername ist bereits in Verwendung.')
+    
+    def validate_email(self, email):
+        if email.data != current_user.email:        
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Für diese Email ist bereits ein Account vorhanden.')
--- fined_be/routes.py
+++ fined_be/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, url_for, flash, redirect, request
 # in a package you use package.module
 from fined_be import app, db, bcrypt
-from fined_be.forms import RegistrationForm, LoginForm
+from fined_be.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from fined_be.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -70,7 +70,19 @@ def logout():
     return redirect(url_for('home'))
 
 
-@app.route('/account')
+@app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Update erfolgreich.', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename="profile_pics/" + current_user.image_file)
+    return render_template('account.html', title='Account',
+        image_file=image_file, form=form)
