03e8884f48712fb09a18bee3b28834446b9ceb47
tomerror1 <tom_klinge@outlook.de>
2020-04-30 20:36:50 +0200

    update profile pictures

--- fined_be/forms.py
+++ fined_be/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
@@ -45,6 +46,7 @@ class UpdateAccountForm(FlaskForm):
         validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('E-Mail', 
         validators=[DataRequired(), Email()])
+    picture = FileField('Update Profilbild', validators=[FileAllowed(['jpg', 'png', 'JPG', 'PNG'])])
     submit = SubmitField('Update')
 
     # only check if data is different from current user
--- fined_be/routes.py
+++ fined_be/routes.py
@@ -1,3 +1,5 @@
+import os
+import secrets
 from flask import render_template, url_for, flash, redirect, request
 # in a package you use package.module
 from fined_be import app, db, bcrypt
@@ -70,11 +72,22 @@ def logout():
     return redirect(url_for('home'))
 
 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    form_picture.save(picture_path)
+    return picture_fn
+
 @app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
     form = UpdateAccountForm()
     if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file 
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.session.commit()
