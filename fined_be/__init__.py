941f751a2711d31d90e7ededdce0678d33401d62
tomerror1 <tom_klinge@outlook.de>
2020-05-01 17:30:23 +0200

    adapt run.py and main-init file for server

--- fined_be/__init__.py
+++ fined_be/__init__.py
@@ -2,20 +2,32 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from fined_be.config import Config
 
 app = Flask(__name__)
 
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 # declace where to login to the login manager
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 
-# has to be down here, to avoid circular errors
-# we are import blueprint instances
-from fined_be.users.routes import users
-from fined_be.main.routes import main
 
-app.register_blueprint(users)
-app.register_blueprint(main)
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+
+    # has to be down here, to avoid circular errors
+    # we are import blueprint instances
+    from fined_be.users.routes import users
+    from fined_be.main.routes import main
+
+    app.register_blueprint(users)
+    app.register_blueprint(main)
+
+    return app
--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 # import from __init__.py
-from fined_be import app
+from fined_be import create_app
+
+app = create_app()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
