f737abf90cf11009116e003d45e3a2bec9addf6e
tomerror1 <tom_klinge@outlook.de>
2020-05-03 14:32:29 +0200

    create models Module, LearningUnit

--- fined_be/models.py
+++ fined_be/models.py
@@ -21,12 +21,21 @@ class User(db.Model, UserMixin):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')" 
 
 
-class Post(db.Model):
+class Module(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(20), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    name = db.Column(db.String(20), unique=False, nullable=False)
+    description = db.Column(db.String(400), unique=False, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default="default_module.jpg")
+    learning_units = db.relationship('LearningUnit', backref='module', lazy=True)
 
     def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted}')" 
+        return f"Module('{self.name}')"
+
+class LearningUnit(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    description = db.Column(db.String(400), unique=False, nullable=True)
+    module_id = db.Column(db.Integer, db.ForeignKey('module.id'), nullable=False)
+
+    def __repr__(self):
+        return f"LearningUnit('{self.name}', '{self.module_id}')"
