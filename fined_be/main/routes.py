c8e0b54d2eebc831055e831d85c062aaad80daac
tomerror1 <tom_klinge@outlook.de>
2020-05-09 15:54:55 +0200

    changed userdata from static to database queries

--- fined_be/main/routes.py
+++ fined_be/main/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, request, Blueprint
 from fined_be.models import Module, LearningUnit
 
+from flask_login import login_user, current_user, logout_user, login_required
+
 main = Blueprint('main', __name__)
 
 posts = [
@@ -20,17 +22,21 @@ posts = [
 
 @main.route('/')
 @main.route('/home')
+@login_required
 def home():
     modules = Module.query.all()
-    return render_template('dashboard.html', modules=modules)
+    user = current_user
+    return render_template('dashboard.html', modules=modules, user=user)
 
 
 @main.route('/investition')
+@login_required
 def investition():
     learning_units = LearningUnit.query.all()
     return render_template('investition.html', learning_units=learning_units)
 
 
 @main.route('/about')
+@login_required
 def about():
     return render_template('about.html')
